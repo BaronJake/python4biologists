8888cdd7d01f719698ff313d8f94f0f1db85397c
baronjake <jake.persons@outlook.com>
2020-06-21 20:45:26 -0500

    chapt 8

--- chapt_7/chapt7_part2.py
+++ chapt_7/chapt7_part2.py
@@ -5,6 +5,10 @@ with open("input/dna.txt") as file:
 
 
 def find_restriction_sites(seq, pattern, restriction_site):
+    """
+    Takes a sequence, a pattern, and where in the pattern the restriction site is
+    Returns indexes of restriction sites
+    """
     sites = []
     for match in re.finditer(pattern, seq):
         site = match.start() + restriction_site
@@ -12,10 +16,13 @@ def find_restriction_sites(seq, pattern, restriction_site):
     return sites
 
 
+# Add the start and end indexes and create a list of restriction sites
 lengths = [0]
+lengths.append(len(sequence))
 lengths.extend(find_restriction_sites(sequence, "A[AGCT]TAAT", 3))
 lengths.extend(find_restriction_sites(sequence, "GC[AG][AT]TG", 4))
-lengths.append(len(sequence))
+
+# get the restriction sites in the right order and subtract to find fragment lengths
 lengths.sort()
 for index in range(1, len(lengths)):
     lengths[index] = lengths[index] - lengths[index - 1]
